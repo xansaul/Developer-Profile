df3bfe6ddba98a5877a8d6498fc3c48073034e47
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2023-09-24 20:31:13 -0600

    navbar background blur and fixed

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,27 +1,32 @@
-import React from 'react'
-import { Link } from 'react-scroll'
+import React from "react";
+import { Link } from "react-scroll";
 
 export const Navbar = () => {
   return (
-    <nav className="h-full md:py-4 py-4 px-5 top-0  md:px-28 backdrop-blur-sm bg-gradient-to-b from-[#00013D]/80 to-transparent flex justify-between">
-      <h1 className="font-bold text-2xl">XanSaul</h1>
-    <ul className="flex gap-5 text-lg md:justify-end sm:justify-start flex-wrap">
-      <li className="cursor-pointer">
-        <Link smooth spy to="experience" href="#experience">
-          Experiencia
-        </Link>
-      </li>
-      <li className="cursor-pointer">
-        <Link smooth spy to="skills"  href="#skills">
-          Skills
-        </Link>
-      </li>
-      <li className="cursor-pointer">
-        <Link smooth spy to="contacts"  href="#experience">
-          Contacto
-        </Link>
-      </li>
-    </ul>
-  </nav>
-  )
-}
+    <nav 
+      className="md:py-4 py-6 sm:px-8 px-4 top-0 md:px-28 
+        backdrop-blur-md bg-[#000126]/70 flex justify-between items-center
+        fixed w-full 
+        "
+    >
+      <h1 className="font-bold md:text-3xl text-2xl">XanSaul</h1>
+      <ul className="flex gap-5 md:text-lg md:justify-end sm:justify-start flex-wrap">
+        <li className="cursor-pointer">
+          <Link smooth spy to="experience" href="#experience">
+            Experiencia
+          </Link>
+        </li>
+        <li className="cursor-pointer">
+          <Link smooth spy to="skills" href="#skills">
+            Skills
+          </Link>
+        </li>
+        <li className="cursor-pointer">
+          <Link smooth spy to="contacts" href="#experience">
+            Contacto
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
