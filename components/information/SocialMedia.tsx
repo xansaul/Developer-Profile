8c9bbfa448fc2476215a7e4ebbacfe48e5bbd8ea
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2024-07-29 22:42:53 -0600

    icons social media

--- components/information/SocialMedia.tsx
+++ components/information/SocialMedia.tsx
@@ -1,49 +1,39 @@
 
 import Image from "next/image";
 import React from "react";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export const SocialMedia = () => {
   return (
     <div className="flex justify-center mt-2 gap-2">
-      
-      <a
-        href="https://github.com/xansaul"
-        target="_blank"
-        title="Github xansaul"
 
-      >
-        <Image
-          src="/svg/github.svg"
-          alt="logo github"
-          width={25}
-          height={25}
-        />
-      </a>
+
+        <a
+          href="https://github.com/xansaul"
+          target="_blank"
+          title="Github xansaul"
+          aria-label="Github"
+        >
+          <FaGithub size={26} />
+          
+        </a>
 
       <a
-        href="https://www.linkedin.com/in/alejandro-saul-huerta-murillo-a25a32b221/"
+        href="https://www.linkedin.com/in/alejandro-saul-huerta-murillo-a16a32b221/"
         target="_blank"
         title="Linkedin xansaul"
+        aria-label="Linkedin"
       >
-        <Image
-          src="/svg/linkedin.svg"
-          alt="logo linkedin"
-          width={25}
-          height={25}
-        />
+        <FaLinkedin size={26} />
       </a>
 
       <a
         href="https://www.instagram.com/xansaul/"
         target="_blank"
         title="Instagram xansaul"
+        aria-label="Instagram"
       >
-        <Image
-          src="/svg/instagram.svg"
-          alt="logo instagram"
-          width={25}
-          height={25}
-        />
+        <FaInstagram size={26} />
       </a>
     </div>
   );
